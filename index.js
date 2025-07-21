a9b9b192434ffe835cb41ecd808c995af968ed4a
nihal1434 <nihalonly772@gmail.com>
2025-07-21 16:37:26 +0530

    improved the overall features and UI with whatever i have learnt till now

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let incrementBtn = document.getElementById("increment-btn");
+let saveBtn = document.getElementById("save-btn");
+let totalEntries= document.getElementById("Total-entry");
+let count = 0;
+let total=0;
+incrementBtn.addEventListener("click", function () {
+  count += 1;
+  countEl.textContent = count;
+});
 
-function increment() {
-    count += 1;
-    countEl.textContent = count;
-}
-
-function save() {
+saveBtn.addEventListener("click", function () {
+  if (count > 0) {
     let countStr = count + " - ";
     saveEl.textContent += countStr;
+    total+=count;
+    totalEntries.textContent =`Total Entries: ${total}`
     countEl.textContent = 0;
     count = 0;
-}
+  }
+});
