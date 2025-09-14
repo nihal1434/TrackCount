a85b0343997d26934911dc2f2b7a481b57988f8a
nihal1434 <nihalonly772@gmail.com>
2025-09-14 17:15:07 +0530

    improved overall look and feel

--- index.js
+++ index.js
@@ -2,32 +2,31 @@ let previousEntry = document.getElementById("previous-entry");
 let countEl = document.getElementById("count-el");
 let incrementBtn = document.getElementById("increment-btn");
 let saveBtn = document.getElementById("save-btn");
-let totalEntries = document.getElementById("total-entry"); // ✅ fixed case
+let totalEntries = document.getElementById("total-entry"); 
 let resetBtn = document.getElementById("reset-btn");
 
 let count = 0;
 let total = 0;
 
-incrementBtn.addEventListener("click", function () {
-  count += 1;
-  countEl.textContent = count;
-});
+incrementBtn.addEventListener("click",function(){
+  count++;
+  countEl.textContent=count;
+})
 
-saveBtn.addEventListener("click", function () {
-  if (count > 0) {
-    let countStr = count + " - ";
-    previousEntry.textContent += countStr;
-    total += count;
-    totalEntries.textContent = `Total Entries: ${total}`;
-    countEl.textContent = 0;
-    count = 0;
+saveBtn.addEventListener("click",function(){
+  if(count>0){
+    let countStr=count+" - ";
+    previousEntry.textContent+=countStr
+    total+=count;
+    totalEntries.textContent=`Total Entries: ${total}`
+    countEl.textContent=0;
+    count=0;
   }
-});
+})
 
-resetBtn.addEventListener("click", function () {
-  previousEntry.textContent = `Previous Entries:`;
-  totalEntries.textContent = `Total Entries: 0`;
-  countEl.textContent = 0;
-  count = 0;
-  total = 0;
-});
+resetBtn.addEventListener("click",function(){
+  previousEntry.textContent=`Previous Entries:`
+  totalEntries.textContent=`Total Entries:0`
+  countEl.textContent=0
+  count=total=0;
+})
